9aebd39e0c16d6669087a16be4360460f8b9114b
rahat14 <rahatshovo6666@gmail.com>
2018-07-03 02:21:55 +0600

    v2.8.3
    added progressbar on webview

--- app/src/main/java/com/metacoders/home/WPPostDetails.java
+++ app/src/main/java/com/metacoders/home/WPPostDetails.java
@@ -1,12 +1,16 @@
 package com.metacoders.home;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.crashlytics.android.Crashlytics;
@@ -14,9 +18,11 @@ import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
 
 public class WPPostDetails extends AppCompatActivity {
+    private   ProgressBar spinner;
 
              TextView title ;
              WebView webview ;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,7 +30,10 @@ public class WPPostDetails extends AppCompatActivity {
         Fabric.with(this,new Crashlytics());
         setContentView(R.layout.viewing_article_in_web);
 
+
         webview = (WebView ) findViewById(R.id.postwebview) ;
+        spinner = (ProgressBar) findViewById(R.id.progressBar1) ;
+        spinner.setVisibility(View.VISIBLE);
         Intent o = getIntent() ;
         int position = getIntent().getExtras().getInt("itemPosition") ;
 
@@ -32,9 +41,23 @@ public class WPPostDetails extends AppCompatActivity {
         Log.e("WpPostDetails", "title is "+ ArticleActivity.mListPost.get(position).getTitle().getRendered()) ;
 
                 webview.getSettings().setJavaScriptEnabled(true);
+
                 webview.loadUrl(ArticleActivity.mListPost.get(position).getLink());
+
                 webview.setWebViewClient(new WebViewClient());
+        Handler handler  = new Handler() ;
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                spinner.setVisibility(View.GONE);
+
+            }
+        },5000);
 
 
     }
+
+
+
 }
+
